ff2a088d1e5561076322856689dd203266ceea4a
Katie Stokes <katie.stokes@hci.utah.edu>
2024-09-06 16:14:27 -0600

    Add TwilioTester project files, can send message to test phone number

---
+++ twilio-tester/src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+import com.twilio.Twilio;
+import com.twilio.converter.Promoter;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import java.net.URI;
+import java.math.BigDecimal;
+
+public class Main {
+    // Find your Account Sid and Token at twilio.com/console
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");;
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+18777804236"), //Trainee phone number
+                new com.twilio.type.PhoneNumber("+18665241385"), //Twilio account number
+                "Hello from Twilio sent programatically")
+                .create();
+
+        System.out.println(message.getSid());
+    }
+}
+
+//// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+//// then press Enter. You can now see whitespace characters in your code.
+//public class Main {
+//    public static void main(String[] args) {
+//        // Press Opt+Enter with your caret at the highlighted text to see how
+//        // IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+//
+//        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+//        for (int i = 1; i <= 5; i++) {
+//
+//            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+//            // for you, but you can always add more by pressing Cmd+F8.
+//            System.out.println("i = " + i);
+//        }
+//    }
+//}
